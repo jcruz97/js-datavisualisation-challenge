e220f3c5e3730ba6f9ec749de149947c9a0d0a67
jcruz97 <cruzjurgen97@gmail.com>
2020-08-17 10:02:26 +0200

    last push

--- js-table/ajax-chart.js
+++ js-table/ajax-chart.js
@@ -57,21 +57,3 @@ let ajChart = new Chart(ctx, {
 
 });
 }
-
-function fetchdata(){
-    $.ajax({
-     url: 'fetch_details.php',
-     type: 'post',
-     success: function(data){
-      // Perform operation on return value
-      alert(data);
-     },
-     complete:function(data){
-      setTimeout(fetchdata,5000);
-     }
-    });
-   }
-   
-   $(document).ready(function(){
-    setTimeout(fetchdata,5000);
-   });
